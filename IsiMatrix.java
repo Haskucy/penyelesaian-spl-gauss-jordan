05aa66f12de81535c478137290ed20ee216e6da9
Rendya Yuschak <rendyayuschak@gmail.com>
2019-09-06 02:44:31 +0700

    add isi matrix

---
+++ IsiMatrix.java
@@ -0,0 +1,37 @@
+public class IsiMatrix {
+	private static int baris = 0;
+	private static int unknown = 0;
+	private static int koefisienMatrixAug = unknown+1;
+	private static int[][] matrixAug = new int[baris][unknown+1];
+
+
+
+	public IsiMatrix(int baris1, int unknown1) {
+		this.baris = baris1;
+		this.unknown = unknown1;
+		this.koefisienMatrixAug = unknown+1;
+		this.matrixAug = new int[baris][unknown+1];
+	}
+
+	public void inputMatrixAug(int barisSaatIni, String koefisienMatrix) {
+		int barisIni = barisSaatIni;
+		String[] koefisien = koefisienMatrix.split(" ");
+
+		for (int i = 0; i < koefisien.length; i++)
+			IsiMatrix.matrixAug[barisIni][i] = Integer.parseInt(koefisien[i]);
+	}
+
+	public void isiMatrix() {
+		for(int i = 0; i< IsiMatrix.baris; i++) {
+			for(int j = 0; j<IsiMatrix.koefisienMatrixAug; j++) {
+				System.out.print(matrixAug[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public void runner() {
+		System.out.println("isiMatrix -> banyak persamaan : " + IsiMatrix.baris + ", banyak unknown : " + IsiMatrix.unknown );
+	}
+
+}
---
+++ ProgramMain.java
@@ -0,0 +1,23 @@
+import program.IsiMatrix;;
+
+public class ProgramMain {
+	public static void main(String[] args) {
+
+		int banyakPersamaan = 3;
+		int banyakUnknown = 4;
+		String[] koefisien = {"1 2 3 4 100", "2 1 0 0 125", "0 4 5 3 200"};
+
+		IsiMatrix program = new IsiMatrix(banyakPersamaan,banyakUnknown);
+
+		for (int i = 0; i < banyakPersamaan;i++)
+			program.inputMatrixAug(i, koefisien[i]);
+
+		program.isiMatrix();
+
+		program.runner();
+
+
+
+	}
+
+}
